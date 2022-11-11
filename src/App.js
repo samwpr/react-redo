089fed5ce44ae71a49062b8690a9d8f00e1e7dfa
samwpr <swpr10x@gmail.com>
2022-11-11 16:49:03 +0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Employee from './components/Employee.js';
+import { useState } from 'react';
+import {v4 as uuidv4} from 'uuid';
+
 
 function App() {
+  const [role, setRole] = useState("Dev");
+  const [employees, setEmloyees] = useState(
+    [
+      {name: "Sam", role:"Dev", img:"https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?"},
+      {name: "Tam", role:"Admin", img:"https://images.pexels.com/photos/3080717/pexels-photo-3080717.jpeg?"},
+      {name: "Jam", role:"Lolster", img:"https://images.pexels.com/photos/4750165/pexels-photo-4750165.jpeg?"},
+      {name: "Sam", role:"Dev", img:"https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?"},
+      {name: "Tam", role:"Admin", img:"https://images.pexels.com/photos/3080717/pexels-photo-3080717.jpeg?"},
+      {name: "Jam", role:"Lolster", img:"https://images.pexels.com/photos/4750165/pexels-photo-4750165.jpeg?"}
+    ]
+  );
+  const showEmployee = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showEmployee ?
+      <>
+        <input type="text" onChange={(e) => setRole(e.target.value)} />
+        <div className="flex flex-wrap justify-center">
+          {employees.map((employee)=>{
+            return(<Employee  key={uuidv4()} name={employee.name} role={employee.role} img={employee.img}/>);
+          })}
+
+        </div>
+      </>
+      : <p>No Access</p> }
     </div>
   );
 }
